90b34958a848ee266ebae34a09718544fb503148
RohanLodhi <rogth7@gmail.com>
2020-06-23 02:58:38 +0530

    added skills animation and footer

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
 const header = document.querySelector('.navbar');
 const links = document.querySelectorAll('a');
+const bars = document.querySelector('.list');
 
 window.addEventListener('scroll', e => {
     if(window.pageYOffset === 0){
@@ -17,9 +18,19 @@ window.addEventListener('scroll', e => {
             link.classList.add('nav-link');
         });
         console.log('scrolled');
+        console.log(pageYOffset)
     }
     if(window.pageYOffset > 2600){
-        list.classList.add('fill');
+        bars.innerHTML = `<li class="js">JavaScript</li>
+        <div class="bar js"></div>
+        <li class="py">Python</li>
+        <div class="bar py"></div>
+        <li class="html">HTML & CSS</li>
+        <div class="bar html"></div>
+        <li class="c">C & C++</li>
+        <div class="bar c"></div>
+        <li class="java">Java</li>
+        <div class="bar java"></div>`
     }
     
 })
